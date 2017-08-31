3a98e13d41d5cdc7c519cd142f1ad803d2b5ecaf
j710328466 <710328466@qq.com>
2017-08-31 20:39:49 +0800

    first commit

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,25 @@
+import originJsonp from 'jsonp'
+
+export default function jsonp (url, data, option) {
+  // 只需要一个问号，其他全是&符号
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
+  return new Promise((resolve, reject) => {
+    originJsonp(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+export function param (data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += '&' + k + '=' + encodeURIComponent(value)
+  }
+  return url ? url.substring(1) : ''
+}
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <div class="text">Jzxer Music</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable";
+  @import "~common/stylus/mixin";
+  @import "/m-header";
+</style>
